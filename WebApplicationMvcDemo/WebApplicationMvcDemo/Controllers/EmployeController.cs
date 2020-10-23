8b66a7d72b6703a71756c3c5b39f6372c97939c5
mre7kor <Megha.Rani@in.bosch.com>
2020-10-23 13:45:58 +0530

    TestCommit

---
+++ WebApplicationMvcDemo/WebApplicationMvcDemo/Controllers/EmployeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMvcDemo.Models;
+
+namespace WebApplicationMvcDemo.Controllers
+{
+    public class EmployeController : Controller
+    {
+        // GET: Employe
+        public ActionResult Details(int id)
+        {
+            EmployeeContext empContext = new EmployeeContext();
+            Employee employee = empContext.Employees.Single(emp => emp.Id == id);
+            return View(employee);
+        }
+
+        public ActionResult EmployeeList()
+        {
+            EmployeeContext empContext = new EmployeeContext();
+           List<Employee> employees = empContext.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
---
+++ WebApplicationMvcDemo/WebApplicationMvcDemo/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMvcDemo.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
