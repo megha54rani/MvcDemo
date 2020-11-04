d706e1668b63ef42ab78314874b8bcd36698b845
mre7kor <Megha.Rani@in.bosch.com>
2020-11-04 17:51:40 +0530

    htmlhelpersdemo

---
+++ WebApplicationMvcDemo/MvcDemoUsingEntityFW/Controllers/HtmlHelpersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemoUsingEntityFW.Controllers
+{
+    public class HtmlHelpersController : Controller
+    {
+        // GET: HtmlHelpers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
